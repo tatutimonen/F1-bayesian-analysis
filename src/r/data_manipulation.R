89f41a608066da6d9e4814b5fb72496e9cc03dc5
mkaarela <miro.kaarela@aalto.fi>
2021-12-01 16:34:39 +0000

    code to run the stan models and transforming the data

---
+++ src/r/data_manipulation.R
@@ -0,0 +1,30 @@
+library(rstan)
+library(loo)
+library(plyr)
+data = read.csv(file ="data/quali_differences_processed.csv")
+data = data[order(data$age),]
+ages = sort(unique(data$age))
+data_differences = c()
+data_ids = c()
+sizes = c()
+for (i in ages){
+  indices = (data$age)==i
+  sizes = c(sizes, length(data$difference[indices]))
+  data_differences = c(data_differences, c(data$difference[indices]))
+}
+
+
+
+data_differences = matrix(0, nrow = max(sizes), ncol = length(sizes))
+k=1
+for(i in 1:length(sizes)){
+  for(j in 1:sizes[i]){
+    data_differences[j,i] = data_differences[k]
+    k = k+1
+  }
+}
+
+stan_data <- list(time = data_differences, N = ncol(data_differences), max_i=nrow(data_differences), I = sizes)
+fit <- stan(file = "src/stan/separate_model.stan", data = stan_data)
+print(fit)
+#loo(fit)
