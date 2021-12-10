f2bdc0b21672e19c1f23628f586bc13d966da262
Admiralbulldog <roopekausi@hotmail.fi>
2021-12-10 04:13:38 +0200

    stuff for plots

--- src/r/data_manipulation.R
+++ src/r/data_manipulation.R
@@ -32,15 +32,15 @@ stan_data <- list(N = N, total_length = nrow(data), time = data$difference, age 
 stan_data_id <- list(N = N, total_length = nrow(data), time = data$difference, age = ages, min_age = min(ages), model_index=data$model_index, 
                      rep_ages = y_rep_ages, rep_length = y_rep_length, driver_id = data$teammateId, driver_count = length(unique(data$teammateId)))
 
-fit_separate <- stan(file = "src/stan/separate_model.stan", data = stan_data)
-fit_hierarchical <- stan(file = "src/stan/hierarchical_model.stan", data = stan_data)
-fit_pooled <- stan(file = "src/stan/pooled_model.stan", data = stan_data)
+#fit_separate <- stan(file = "src/stan/separate_model.stan", data = stan_data)
+#fit_hierarchical <- stan(file = "src/stan/hierarchical_model.stan", data = stan_data)
+#fit_pooled <- stan(file = "src/stan/pooled_model.stan", data = stan_data)
 
-fit_separate_dprior <- stan(file = "src/stan/separate_model_dprior.stan", data = stan_data)
-fit_hierarchical_dprior <- stan(file = "src/stan/hierarchical_model_dprior.stan", data = stan_data)
-fit_pooled_dprior <- stan(file = "src/stan/pooled_model_dprior.stan", data = stan_data)
+#fit_separate_dprior <- stan(file = "src/stan/separate_model_dprior.stan", data = stan_data)
+#fit_hierarchical_dprior <- stan(file = "src/stan/hierarchical_model_dprior.stan", data = stan_data)
+#fit_pooled_dprior <- stan(file = "src/stan/pooled_model_dprior.stan", data = stan_data)
 
-#fit_separate_id <- stan(file = "src/stan/separate_model_ids.stan", data = stan_data_id, iter=6000)
+fit_separate_id <- stan(file = "src/stan/separate_model_ids.stan", data = stan_data_id, iter=6000)
 #fit_hierarchical_id <- stan(file = "src/stan/hierarchical_model_ids.stan", data = stan_data_id, iter=5000)
 #fit_pooled_id <- stan(file = "src/stan/pooled_model_ids.stan", data = stan_data_id)
 
@@ -54,13 +54,13 @@ llfun <- function(data_i, draws) {
 
 #loo_func_separate <- loo(llfun, data = Data, draws = posterior_draws, r_eff = NA)
 
-loo_mat_separate <- loo(extract_log_lik(fit_separate))
-loo_mat_hierarchical <- loo(extract_log_lik(fit_hierarchical))
-loo_mat_pooled <- loo(extract_log_lik(fit_pooled))
+#loo_mat_separate <- loo(extract_log_lik(fit_separate))
+#loo_mat_hierarchical <- loo(extract_log_lik(fit_hierarchical))
+#loo_mat_pooled <- loo(extract_log_lik(fit_pooled))
 
-loo_mat_separate_dprior <- loo(extract_log_lik(fit_separate_dprior))
-loo_mat_hierarchical_dprior <- loo(extract_log_lik(fit_hierarchical_dprior))
-loo_mat_pooled_dprior <- loo(extract_log_lik(fit_pooled_dprior))
+#loo_mat_separate_dprior <- loo(extract_log_lik(fit_separate_dprior))
+#loo_mat_hierarchical_dprior <- loo(extract_log_lik(fit_hierarchical_dprior))
+#loo_mat_pooled_dprior <- loo(extract_log_lik(fit_pooled_dprior))
 
 #loo_mat_separate_id <- loo(extract_log_lik(fit_separate_id))
 #loo_mat_separate_id_known <- loo(extract(fit_separate_id, pars='log_lik_id')$log_lik_id)
@@ -79,28 +79,33 @@ loo_mat_pooled_dprior <- loo(extract_log_lik(fit_pooled_dprior))
 #yrep_hierarchical_id <- extract(fit_hierarchical_id, pars='yrep')$yrep
 #yrep_hierarchical_id_known <- extract(fit_hierarchical_id, pars='yrep_id')$yrep_id
 
-#ppc_dens_overlay_grouped(y, yrep_hierarchical[1:50,], y_rep_groups)
+#ppc_dens_overlay_grouped(y, yrep_separate_id_known[1:50,], y_rep_groups) + theme_bw(base_size=18)
 
 #mu_separate_id <- extract(fit_separate_id, pars='mu')$mu
 #mu_hierarchical <- extract(fit_hierarchical, pars='mu')$mu
+mu <- extract(fit_separate_id, pars='mu')$mu
 
 num_ages = length(unique(ages))
-probs_separate_id = vector(mode= 'numeric', length = num_ages)
-probs_hierarchical = vector(mode= 'numeric', length = num_ages)
+probs = vector(mode= 'numeric', length = num_ages)
+probs = vector(mode= 'numeric', length = num_ages)
 
 for (i in 1:num_ages) {
-  mu1 = mu_hierarchical[,i]
+  mu1 = mu[,i]
   prob = 1
   for (j in 1:num_ages) {
     if (j != i) {
-      mu2 = mu_hierarchical[,j]
-      prob = prob * (sum(mu[,j]>mu[,i])/length(mu[,1]))
+      mu2 = mu[,j]
+      prob = prob * (sum(mu2>mu1)/length(mu1))
     }
   }
-  probs_hierarchical[i] = prob
+  probs[i] = prob
 }
 
-probs_df <- data.frame(probs_hierarchical/sum(probs_hierarchical), sort(unique(ages)))
+probs_df <- data.frame(probs/sum(probs), sort(unique(ages)))
 colnames(probs_df) <-c('prob', 'age')
 
 #print(fit_separate, pars=c('mu', 'sigma'))
+#arr <- as.array(fit_separate_id, pars='mu')
+#mcmc_intervals(arr)
+#params_a <- as.array(fit_separate_id, pars=c('a[1]', 'a[3]', 'a[4]', 'a[11]', 'a[18]', 'a[21]', 'a[30]', 'a[57]', 'a[123]', 'a[141]', 'a[155]', 'a[131]'))
+#mcmc_intervals(params_a) + scale_y_discrete(labels= c('Schumacher', 'par'))
